81022c4a967ccd44ac217002222c61b32818e586
mtriche128 <mtriche128@gmail.com>
2016-04-27 22:26:40 -0600

    added encoder

---
+++ examples/encoder_example/encoder_example.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "codec/codec.h"
+using namespace std;
+
+int main(void)
+{
+	SymbolEncoder encoder(1,0); // initialize a 1 bit-per-symbol decoder
+	queue<bool> bitqueue;
+	queue<int> symqueue;
+	
+	bitqueue.push(0);
+	bitqueue.push(1);
+	bitqueue.push(1);
+	
+	encoder.push(bitqueue,symqueue);
+	
+	bitqueue = queue<bool>();
+	bitqueue.push(0);	
+
+	encoder.push(bitqueue,symqueue);
+	
+	while(!symqueue.empty())
+	{
+		cout << symqueue.front() << " ";
+		symqueue.pop();
+	}
+		
+	cout << endl;
+
+	return 0;
+}
+
